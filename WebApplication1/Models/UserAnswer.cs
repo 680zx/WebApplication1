b97866f8e03bac9346586395f64c22942fedfdfd
680zx <stas.lekontsv@gmail.com>
2021-09-06 08:08:56 +0400

    Add check atributes, clean code

--- WebApplication1/Models/UserAnswer.cs
+++ WebApplication1/Models/UserAnswer.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
     public class UserAnswer
     {
+        [Required(ErrorMessage = "Enter your name")]
         public string Name { get; set; }
 
         public int Points { get; set; }
